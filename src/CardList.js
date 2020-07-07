b767f80712e17a1c40eff99f135352ec0b1faacb
Emanuele Ercoli <emanuele@recode.digital>
2020-07-08 00:22:34 +0200

    Raffinamento arrow function

--- src/CardList.js
+++ src/CardList.js
@@ -1,27 +1,23 @@
-import React, {Fragment, useState} from 'react';
+import React, {Fragment} from 'react';
 import Card from './Card';
 
 const CardList = ({negozi, categorie}) => {
 
-	const cardsArray = negozi.map((negozio,i) =>{
+	const cardsArray = negozi.map((negozio,i) =>
 
-		//setCat( categorie.filter(categoria => {return categoria.id === negozio.id_categoria}) );
-		//console.log(cat2[0].nome);
-
-			return (
-				<Card 
-					key={i} 
-					nome={negozio.nome} 
-					categoria={
-						categorie
-						.filter(categoria => categoria.id === negozio.id_categoria)
-						.map(categorieFiltrate => (categorieFiltrate.nome))
-					}
-					url_logo={negozio.url_logo} 
-					citta={negozio.citta}
-				/>
-			);
-	});
+		<Card 
+			key={i} 
+			nome={negozio.nome} 
+			categoria={
+				categorie
+				.filter(categoria => categoria.id === negozio.id_categoria)
+				.map(categorieFiltrate => categorieFiltrate.nome)
+			}
+			url_logo={negozio.url_logo} 
+			citta={negozio.citta}
+		/>
+			
+	);
 
 
 	return (
