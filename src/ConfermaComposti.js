e5a0d6a69f4e05c64836b8a3a3733621b60e2fbb
Emanuele Ercoli <emanuele@recode.digital>
2020-07-24 17:53:25 +0200

    Avanzamento Form ConfermaComposti

--- src/ConfermaComposti.js
+++ src/ConfermaComposti.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
+import { useForm } from "react-hook-form"
 
-import { Dropdown } from 'semantic-ui-react'
+import { Dropdown, Step, Icon, Form, Button} from 'semantic-ui-react'
+import SezioneBoxed from './SezioneBoxed';
 
 const ConfermaComposti = (props) => {
 
@@ -8,6 +10,11 @@ const ConfermaComposti = (props) => {
     const [componentiArticolo, setComponentiArticolo] = useState([]);
     const [articoliOrdinatiComposti, setArticoliOrdinatiComposti] = useState([]);
     const [articoliComposti, setArticoliComposti] = useState([]);
+    const [idOrdine, setIdOrdine] = useState('');
+    const [campoFormModificato, setCampoFormModificato] = useState('');
+
+    const { register, handleSubmit, setValue, watch, errors} = useForm();
+    
 
     useEffect(() => {
 
@@ -20,6 +27,7 @@ const ConfermaComposti = (props) => {
                 setAssociazioniComponenteArticolo(json.get_associazioni_componente_articolo_da_negozio);
                 setArticoliOrdinatiComposti(json.get_articoli_ordinati_composti);
                 setArticoliComposti(json.get_articoli_composti_da_articoli_ordinati);
+                setIdOrdine(json.get_ordine.id);
                 console.log(json)
                 console.log(props.match.params.id_ordine)
                 // setInfoNegozio(json.get_negozio);
@@ -34,48 +42,87 @@ const ConfermaComposti = (props) => {
 
     }, []);
 
+    //Per registrare valore Dropdown Semantic UI
+    const handleChange = (e, { name, value }) => {
+        setCampoFormModificato(name);
+        setValue(name, value);
+    }
+
     const stampaSelectComponenti = articoliOrdinatiComposti
-                                    .map( articoloOrdinatoComposto => {
-                                        console.log('ciao2')
-                                        var arrayOpzioniComponenti = [];
+                                    .map( (articoloOrdinatoComposto, i) => {
+                                        let nome='';
+                                        let descrizione='';
+                                        let arrayOpzioniComponenti = [];
+                                        let quantita = articoloOrdinatoComposto.quantita;
                                         articoliComposti
                                             .filter( articoloComposto => articoloComposto.id === articoloOrdinatoComposto.id_articolo)
                                             .map(articoloCompostoFiltrato => {
-                                                                            console.log('ciao3')
+                                                                            nome = articoloCompostoFiltrato.nome;
+                                                                            descrizione = articoloCompostoFiltrato.descrizione;
                                                                             associazioniComponenteArticolo
                                                                                 .filter( associazioneComponenteArticolo => associazioneComponenteArticolo.id_articolo === articoloCompostoFiltrato.id)
-                                                                                .map( associazioneComponenteArticoloFiltrato => {
-                                                                                                                                    console.log('ciao4')
-                                                                                                                                    componentiArticolo
+                                                                                .map( associazioneComponenteArticoloFiltrato => componentiArticolo
                                                                                                                                     .filter( componenteArticolo => componenteArticolo.id === associazioneComponenteArticoloFiltrato.id_componente)
-                                                                                                                                    .map(componenteFiltrato => {
-                                                                                                                                        console.log('ciao')
-                                                                                                                                            arrayOpzioniComponenti.push({key: componenteFiltrato.id, text: componenteFiltrato.nome, value: componenteFiltrato.id})
-                                                                                                                                        }
-                                                                                                                                    )}
+                                                                                                                                    .map( componenteFiltrato => arrayOpzioniComponenti.push({key: componenteFiltrato.id, text: componenteFiltrato.nome, value: componenteFiltrato.id}) )
                                                                                 )}
                                             )
+                                            let arraycomp = []            
+                                                for (let index = 0; index < quantita; index++) {
+                                                    arraycomp.push(
+                                                        <div className="pa3 ba b--black-30 br3 mb3" key={articoloOrdinatoComposto.id+index}>
+                                                            <h4>{index+1} - {nome}</h4>
+                                                            <p>{descrizione}</p>
+                                                            <Dropdown onChange={handleChange} name={"componenti-"+articoloOrdinatoComposto.id_articolo+"-"+index} id={"componenti"+articoloOrdinatoComposto.id_articolo} placeholder='Seleziona le opzioni per questo prodotto' fluid multiple selection clearable options={arrayOpzioniComponenti} required/>
+                                                        </div> 
+                                                    )
+                                                    
+                                                    
+                                                } 
+                                            return arraycomp                                           
+                                                
 
-                                        return <Dropdown  name={"componenti"+articoloOrdinatoComposto.id_articolo} id={"componenti"+articoloOrdinatoComposto.id_articolo} placeholder='Seleziona le opzioni per questo prodotto' fluid multiple selection clearable options={arrayOpzioniComponenti}/>
                                     }
                     
                                     )
 
+    
 
-        // associazioniComponenteArticolo
-        //     .filter( associazione => associazione.id_articolo === id)
-        //     .map( associazioneFiltrata => componentiArticolo
-        //                                     .filter( componente => componente.id === associazioneFiltrata.id_componente)
-        //                                     .map( componenteFiltrato => arrayOpzioniComponenti.push({key: componenteFiltrato.nome, text: componenteFiltrato.nome, value: componenteFiltrato.id}))
-        //     )
-        // if(arrayOpzioniComponenti.length>0)
-        // return (<Dropdown onChange={handleChange} name={"componenti"+id} id={"componenti"+id} placeholder='Seleziona le opzioni per questo prodotto' fluid multiple selection clearable options={arrayOpzioniComponenti}/>)
+    useEffect( () => {
+        let prova = 
+        register(campoFormModificato);
+    },[register])
+
+    const onSubmit = data => {
+        console.log(data)
+    }
 
     return (
-        <div className="mt6">
-            {stampaSelectComponenti}
+        <SezioneBoxed className="mt6">
+            <Step.Group stackable="tablet" size="tiny">
+                <Step completed >
+                    <Icon name='credit card' />
+                    <Step.Content>
+                        <Step.Title>1 - Scegli</Step.Title>
+                        <Step.Description>Scegli cosa vuoi ordinare</Step.Description>
+                    </Step.Content>
+                </Step>
+                <Step active>
+                    <Icon name='clipboard check' />
+                    <Step.Content>
+                        <Step.Title>2 - Scegli le opzioni</Step.Title>
+                        <Step.Description>Scegli le opzioni o i componenti per questi prodotti</Step.Description>
+                    </Step.Content>
+                </Step>
+            </Step.Group>
+            <p className="tc">Alcuni dei tuoi prodotti hanno delle opzioni o dei componenti fra cui scegliere</p>
+            <Form onSubmit={handleSubmit(onSubmit)} nome="formComponenti" id="formComponenti">
+                {stampaSelectComponenti}
+                <Button color="green" type='submit'>Invia ora il tuo ordine</Button>
+                <p>{campoFormModificato}</p>
+            </Form>
             
-        </div>
+            
+        </SezioneBoxed>
     )
 }
 
