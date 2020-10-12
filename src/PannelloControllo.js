72ee9b83e5cb1283f5c5fbfac110754a43b60b45
Emanuele Ercoli <emanuele@recode.digital>
2020-10-12 19:36:24 +0200

    Lista Prodotti Pannello di Controllo

--- src/PannelloControllo.js
+++ src/PannelloControllo.js
@@ -2,6 +2,13 @@ import React, { useState, Fragment, useEffect } from 'react'
 import { useHistory } from "react-router-dom";
 import LoaderOS from './LoaderOS';
 
+import { Button, Icon, Menu, Dropdown } from 'semantic-ui-react'
+import ListaArticoli from './ListaArticoli'
+
+import { HashLink as Link } from 'react-router-hash-link';
+
+
+
 const PannelloControllo = ({}) => {
 
     const [visibilitaLoader, setVisibilitaLoader] = useState(true);
@@ -43,10 +50,60 @@ const PannelloControllo = ({}) => {
   
       }, [])
 
+      const stampaSubmenuCategorieDesktop = categorieArticoli => (
+        categorieArticoli.map( categoria => 
+                                <Menu.Item as={Link}
+                                    key={categoria.id}
+                                    to={"#categoria-"+categoria.nome}  
+                                    name= {categoria.nome}
+                                >
+                                    {categoria.nome}
+                                </Menu.Item>)
+    )
+
+    const opzioniCategorieMobile = (categorieArticoli) => {
+        let arrayOpzioniCategorie = [];
+        categorieArticoli.map( categoria => arrayOpzioniCategorie.push({
+                                                                    key: categoria.id,
+                                                                    text: categoria.nome,
+                                                                    value: categoria.nome
+                                                                }
+                                                                ))
+        return arrayOpzioniCategorie;
+    }
+
+    const handleChange = (e, { value }) => {
+        var elmnt = document.getElementById("categoria-"+value);
+        elmnt.scrollIntoView();
+    }
+
     return (
     
         <Fragment>
             <LoaderOS visibilita={visibilitaLoader} frase="Stiamo raccogliendo tutte le informazioni..."/>
+
+            <div className="w-100 flex flex-row items-start justify-center mt6">
+                <div className="w-20 dn flex-l items-start justify-center pt6 pl2" style={{'position' : "sticky", "top" : "0"}}>
+                    <Menu vertical>
+                        <Menu.Item active><b className="f3">Categorie</b></Menu.Item>
+                        {stampaSubmenuCategorieDesktop(categorieArticoli)}  
+                    </Menu>
+                </div>
+                <div className="w-100 w-80-l">
+                    <div className="w-100 flex items-center justify-end pr2 pt2 dn-l" style={{"position" : "sticky", "top" : "100px", "zIndex":'1'}}>
+                        <Dropdown
+                            placeholder='Vai alla categoria...'
+                            className="dn-l"
+                            onChange={handleChange}
+                            selection
+                            options={opzioniCategorieMobile(categorieArticoli)}
+                        />    
+                    </div>
+                    
+                    <ListaArticoli idNegozio={JSON.parse(localStorage.getItem('infoUtente')).id_negozio} abbonamentoUtente={infoUtenteNegozio.livello} articoli={articoli} categorieArticoli={categorieArticoli} componentiArticolo={componentiArticolo} associazioniComponenteArticolo={associazioniComponenteArticolo} />
+                </div>
+
+            </div>
 
         </Fragment>
 
