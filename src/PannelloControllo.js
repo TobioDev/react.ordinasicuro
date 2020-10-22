c18cb400bad9f4fefb12f1153e856e34d8d335d3
Emanuele Ercoli <emanuele@recode.digital>
2020-10-23 01:12:51 +0200

    Modifica Bottone Gestisci Componenti

--- src/PannelloControllo.js
+++ src/PannelloControllo.js
@@ -108,7 +108,7 @@ const PannelloControllo = ({setLoggato}) => {
                             <Button positive onClick={() => historyPush('/aggiungi-articolo/')}>Aggiungi Prodotto</Button>
                             <Button>Gestisci Ordini</Button>
                             <Button>Gestisci Categorie</Button>
-                            <Button onClick={() => historyPush('/gestione-componenti/')}>Gestisci Componenti</Button>
+                            <Button color="blue" onClick={() => historyPush('/gestione-componenti/')}>Gestisci Componenti</Button>
                             <Button>Gestisci il tuo Profilo</Button>
                         </Button.Group>
                     </div>
